e9d558b56ba48ae4c648c095fe1cfdeed98c8701
testerdu59 <monsite059@gmail.com>
2019-06-13 01:15:24 +0200

    maj1306

--- app/src/main/java/projet/maxime/loupgarouandroid/Attribution.kt
+++ app/src/main/java/projet/maxime/loupgarouandroid/Attribution.kt
@@ -1,13 +1,23 @@
 package projet.maxime.loupgarouandroid
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import org.json.JSONArray
+import kotlinx.android.synthetic.main.activity_attribution.*
 
 class Attribution : AppCompatActivity() {
 
     private lateinit var bdd : CartesDB
     private var jsonArray = JSONArray()
+
+    var isStarted = false
+    var progressStatus = 0
+    var handler: Handler? = null
+    var secondaryHandler: Handler? = Handler()
+    var primaryProgressStatus = 0
+    var secondaryProgressStatus = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +27,59 @@ class Attribution : AppCompatActivity() {
 
         this.initCompo()
         jsonArray = JSONArray(intent.getStringExtra("listeNoms"))
-        //https://www.androidly.net/327/android-progressbar-using-kotlin
 
+        btnProgressBarSecondary.setOnClickListener {
+            primaryProgressStatus = 0
+            secondaryProgressStatus = 0
+
+            Thread(Runnable {
+                while (primaryProgressStatus < 100) {
+                    primaryProgressStatus += 1
+
+                    try {
+                        Thread.sleep(1000)
+                    } catch (e: InterruptedException) {
+                        e.printStackTrace()
+                    }
+
+                    startSecondaryProgress()
+                    secondaryProgressStatus = 0
+
+                    secondaryHandler?.post {
+                        progressBarSecondary.progress = primaryProgressStatus
+                        textViewPrimary.text = "Complete $primaryProgressStatus% of 100"
+
+                        if (primaryProgressStatus == 100) {
+                            textViewPrimary.text = "All tasks completed"
+                        }
+                    }
+                }
+            }).start()
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun startSecondaryProgress() {
+        Thread(Runnable {
+            while (secondaryProgressStatus < 100) {
+                secondaryProgressStatus += 1
+
+                try {
+                    Thread.sleep(10)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+
+                secondaryHandler?.post {
+                    progressBarSecondary.secondaryProgress = secondaryProgressStatus
+                    textViewSecondary.text = "Current task progress\n$secondaryProgressStatus% of 100"
+
+                    if (secondaryProgressStatus == 100) {
+                        textViewSecondary.text = "Single task complete."
+                    }
+                }
+            }
+        }).start()
     }
 
     private fun initCompo() {
